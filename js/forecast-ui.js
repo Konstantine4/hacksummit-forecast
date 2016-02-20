34c721ae2507e93e8df8967c7e6d679995205b79
Turo Nylund <turo.nylund@outlook.com>
2016-02-20 21:00:03 +0000

    improfed frontpage

--- js/forecast-ui.js
+++ js/forecast-ui.js
@@ -9,11 +9,58 @@
     
     function ForecastController($http) {
         
+        // variables
         var vm = this;
+        vm.conditions = [];
+        vm.periods = [];
+        vm.actions = [];
+        vm.selectedConfigs = {};
+        vm.conditionMessage = "";
+        vm.actionMessage = "";
+        vm.periodMessage = "IN NEXT";
         
-        vm.mymessage = "my message";
-        
+        // methods
         vm.submitConfiguration = submitConfiguration;
+        vm.selectPeriod = selectPeriod;
+        vm.selectCondition = selectCondition;
+        vm.selectAction = selectAction;
+        
+        // calling init
+        init();
+        
+        function init() {
+            vm.conditions = getConditions();
+            vm.periods = getPeriods();
+            vm.actions = getActions();
+            selectCondition(1);
+            selectAction(1);
+            selectPeriod(1);
+        }
+        
+        function selectCondition(id) {
+            var item = vm.conditions.filter(function(x) { return x.id == id })[0];
+            vm.selectedConfigs["condition"] = item;
+            changeConditionMessage(item.message)
+            
+        }
+        
+        function selectAction(id) {
+            var item = vm.actions.filter(function(x) { return x.id == id })[0];
+            vm.selectedConfigs["action"] = item;
+            changeActionMessage(item.message);
+        }
+        
+        function selectPeriod(id) {
+            vm.selectedConfigs["period"] = vm.periods.filter(function(x) { return x.id == id })[0];
+        }
+        
+        function changeConditionMessage(message) {
+            vm.conditionMessage = message;
+        }
+        
+        function changeActionMessage(message) {
+            vm.actionMessage = message;
+        }
         
         function submitConfiguration() {
             var url = "http://169.45.221.144:3000/newConfig/"
@@ -35,7 +82,31 @@
                 .error(function(err){
                     console.log(err);
                 });
-            }
+        }
+        
+        function getConditions() {
+            var array = [];
+            array.push({ id: 1, title: "RAIN", message: "IF IT'S GOING TO", icon: "wi wi-raindrops"});
+            array.push({ id: 1, title: "SUNNY", message: "IF IT'S GOING TO", icon: "wi wi-day-sunny"});
+            array.push({ id: 2, title: "SNOW", message: "IF IF'S GOING TO", icon: "wi wi-snow"});
+            
+            return array;
+        }
+        
+        function getActions() {
+            var array = [];
+            array.push({ id: 1, title: "WEBHOOK", message: "NOTIFY ME BY", icon: "fa fa-cloud"});
+            array.push({ id: 2, title: "EMAIL", message: "NOTIFY ME BY", icon: "fa fa-envelope"});
+            
+            return array;
+        }
+        
+        function getPeriods() {
+            var array = [];
+            array.push({ id: 1, unit: "HOURS", value: 1});
+            
+            return array;
+        }
         
     }
 })();
