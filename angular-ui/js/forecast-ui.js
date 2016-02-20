96baa42becdfb929803ed0bca275f1dc8d93b401
Turo Nylund <turo.nylund@outlook.com>
2016-02-20 15:45:20 +0000

    user submit works

--- angular-ui/js/forecast-ui.js
+++ angular-ui/js/forecast-ui.js
@@ -5,17 +5,37 @@
     .module("forecastApp")
     .controller("ForecastController", ForecastController);
     
-    ForecastController.$inject = [];
+    ForecastController.$inject = ["$http"];
     
-    function ForecastController() {
+    function ForecastController($http) {
         
         var vm = this;
         
         vm.mymessage = "my message";
         
-        vm.apply = apply;
+        vm.submitConfiguration = submitConfiguration;
         
-        function apply() {}
+        function submitConfiguration() {
+            var url = "http://169.45.221.144:3000/newConfig/"
+            var data = {
+                "userId": "turouser2",
+                "config": "forecast",
+                "weatherCondition": "Snow",
+                "lat":100,
+                "lon": 100,
+                "notificationTime": "asap",
+                "predictionTime": "9"
+            }
+                        
+            $http.post(url, data)
+                .success(function(data){
+                    // $scope.myData = data;
+                    alert(data);
+                })
+                .error(function(err){
+                    console.log(err);
+                });
+            }
         
     }
 })();
