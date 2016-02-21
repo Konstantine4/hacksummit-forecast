16768cc1804ec10b554271dd459e1ccb50534df7
Koding <ernanirst@ernanirst.koding.io>
2016-02-21 11:23:48 -0600

    Created notification endpoint for client

--- private/server/app_modules/db_handler.js
+++ private/server/app_modules/db_handler.js
@@ -23,29 +23,53 @@ var insertDocument = function(db, config, callback) {
 }
 
 /* Get alL products from Database ****************************************/
-var getProducts = function(res){
+var getProducts = function(callback){
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
-        getAllDocuments(db, res);
+        getAllDocuments(db, callback);
     });
 }
 
-var getAllDocuments = function(db, res){
-    var configs = [];
+var getAllDocuments = function(db, callback){
+    var products = [];
     var cursor = db.collection('products').find();
     cursor.each(function(err, doc) {
       assert.equal(err, null);
       if (doc !== null) {
-        //console.log(doc);
-        
         //add to array
-        configs.push(doc);
+        products.push(doc);
       } else {
          db.close();
-         res.send(JSON.stringify(configs));
+         callback(products);
       }
    });
 }
 
+/* Decrease Products Prices ****************************************/
+var decreaseProductPrice = function(product){
+    var ObjectId = require('mongodb').ObjectID;
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        
+        //console.log("Connection opened");
+        console.log("New Price: " + product.basePrice*(1-product.priceVariation));
+        var newPrice = product.basePrice*(1-product.priceVariation);
+        db.collection('products').updateOne(
+            {
+                "_id": new ObjectId(product._id)
+            },
+            {
+                $set: { "price": newPrice, "lastPriceUpdate" : Math.floor(Date.now() / 1000)}
+            }, function(err, results) {
+                //console.log(results);
+                db.close();
+                console.log("OF COURSE ITS COPY PASTE TURO!!!");
+            }
+        );
+
+    });
+}
+
 module.exports.insertConfig = insertConfig;
 module.exports.getProducts = getProducts;
+module.exports.decreaseProductPrice = decreaseProductPrice;
